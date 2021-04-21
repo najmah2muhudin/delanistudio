4ec8095357073fc7b512cd1613494f917aad1981
najmamuhudin <najmahmohaaa@gmail.com>
2021-04-21 11:34:11 +0300

    addupdated jquery

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#img1").click(function(){
+        $(".showing").fadeToggle();
+        $("#design").fadeToggle();
+    });
+});
+
+ $("#imge2").click(function(){
+     $(".shw").fadeToggle();
+     $("#dev").fadeToggle();
+ });
+ $("#image3").click(function(){
+     $(".show").fadeToggle();
+     $("#product").fadeToggle();
+ })
+
